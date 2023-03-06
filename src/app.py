d4dccb524942ba81df7efd7776a0693e1a9cd41a
Maxwell Spadafore <maxspad@umich.edu>
2023-03-05 21:02:08 -0500

    silly waveform working

--- src/app.py
+++ src/app.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 import time
 import psutil
 import shutil
+import numpy as np
+import sys
+import queue 
 
 st.title('Vinyl Recorder')
 
@@ -34,16 +37,50 @@ def get_res_stats():
         'hdd': int(float(hdd.used) / hdd.free * 100)
     }
 
+q = queue.Queue()
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
+    # print('qlen', q.qsize())
 
 res_cols = st.columns(3)
 res_labs = ['CPU:', 'Mem:', 'HDD:']
 res_indexes = ['cpu','mem','hdd']
 current_res = get_res_stats()
 res_progs = [c.progress(current_res[res_t], text=lab) for c, res_t, lab in zip(res_cols, res_indexes, res_labs)]
+vol_prog_left = st.progress(0.0, text='Left')
+vol_prog_right = st.progress(0.0, text='Right')
+record_or_stop = st.empty()  
 
-record_clicked = st.button('🔴 Record')
+wave_box = st.empty()
+
+record_clicked = record_or_stop.button('🔴 Record')
+
 if record_clicked:
     channels = 2 # stereo
+    stop_clicked = record_or_stop.button('⬛ Stop')
+    with sd.InputStream(samplerate=fs,
+                        channels=channels, callback=callback):
+        while True:
+            if stop_clicked: 
+                break
+            sound_data = [np.zeros((0,2))]
+            while not q.empty():
+                sound_data.append(q.get())
+            sound_data = np.vstack(sound_data)
+            if sound_data.shape[0] > 0:
+                vol_prog_left.progress(sound_data[:,0].max(), text='Left')
+                vol_prog_right.progress(sound_data[:,1].max(), text='Right')
+                wave_box.line_chart(sound_data[:,0]) # TODO make this update less frequently
+
+            current_res = get_res_stats()
+            for p, idx, lab in zip(res_progs, res_indexes, res_labs):
+                p.progress(current_res[idx], text=lab)
+
+            sd.sleep(100)
+'''
     rec = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
     prog = st.progress(0.0, text='Recording')
     with st.empty():
@@ -66,3 +103,5 @@ if record_clicked:
     st.pyplot(f)
 
     st.audio(rec.T, sample_rate=fs)
+
+'''
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -it -e "PULSE_SERVER=${PULSE_SERVER}" -v /mnt/wslg/:/mnt/wslg -v $PWD/src:/app -p 8501:8501 imax32/vinyl-recorder $1
+docker run --rm -it -e "PULSE_SERVER=${PULSE_SERVER}" -v /mnt/wslg/:/mnt/wslg -v $PWD/src:/app -p 8501:8501 imax32/vinyl-recorder $1
