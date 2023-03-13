3f499cc3f71d332a5c80bd01b43123c15f42ba91
Maxwell Spadafore <maxspad@umich.edu>
2023-03-12 23:13:04 -0400

    switching to mamba env

--- build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Building..."
-docker build -t imax32/vinyl-recorder .
--- start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run --rm -it -e "PULSE_SERVER=${PULSE_SERVER}" -v /mnt/wslg/:/mnt/wslg -v $PWD/src:/app -p 8501:8501 imax32/vinyl-recorder $1
